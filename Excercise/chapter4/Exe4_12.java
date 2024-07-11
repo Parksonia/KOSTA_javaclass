295cc422371861fac8af77a5432c9f5a1f20621c
Parksonia <140988009+Parksonia@users.noreply.github.com>
2024-07-11 20:12:45 +0900

    Update Exe4_12.java

--- Excercise/chapter4/Exe4_12.java
+++ Excercise/chapter4/Exe4_12.java
@@ -34,3 +34,21 @@ public class Exe4_12 {
 //8*1=8 9*1=9
 //8*2=16 9*2=18
 //8*3=24 9*3=27
+
+
+
+		// for (int i = 2; i < 10; i += 3) {// 2,5,8 큰 틀의 줄바꿈
+
+		// 	for (int j = 1; j <= 3; j++)  {
+
+				                                 
+		// 			for (int k = i; k <= (i + 2); k++) {// 한줄인 거임
+		// 		System.out.print(String.format("%dx%d=%d\t", k, j, k * j));
+
+		// 		}
+
+		// 		System.out.println();
+		// 	}
+
+		// 	System.out.println();
+		// }
